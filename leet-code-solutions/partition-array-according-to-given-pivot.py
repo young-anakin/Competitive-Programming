82fb4fb9e4489e4cdf51f037d308d1e8a6185c57
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2023-12-07 16:02:51 +0300

    Added solution for Partition Array According to Given Pivot

--- leet-code-solutions/partition-array-according-to-given-pivot.py
+++ leet-code-solutions/partition-array-according-to-given-pivot.py
@@ -1,22 +1,15 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         before = []
+        after = []
+        pivotList = []
         count = 0
-        nums.append('a')
-        a = 0
-        while nums[a] != 'a':
-            # print(nums[a], pivot)
-            if nums[a] == pivot:
-                # print(a)
-                nums.pop(a)
-                count +=1
-            elif nums[a] < pivot:
-                # print("hey")
-                before.append(nums.pop(a))
-                # continue
+        for a in range(0, len(nums)):
+            if nums[a] < pivot:
+                before.append(nums[a])
+            elif nums[a]  == pivot:
+                pivotList.append(pivot)
             else:
-                a+=1
-        for a in range(0, count):
-            before.append(pivot)
-        nums.pop(-1)
-        return before + nums
+                after.append(nums[a])
+
+        return before + pivotList + after
