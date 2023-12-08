a6a79ec12643b39d1218ef0bc7aab73d61813a96
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2023-12-08 12:21:05 +0300

    Added solution for Find Players With Zero or One Losses

---
+++ leet-code-solutions/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        notLost = []
+        oneLoss = []
+        frequency = {}
+        for i in range(0, len(matches)):
+            if matches[i][0]  not in frequency:
+                frequency[matches[i][0]] = 0
+            if matches[i][1] not in frequency:
+                frequency[matches[i][1]] = 1
+            else:
+                frequency[matches[i][1]] += 1
+        for key, values in frequency.items():
+            if values == 0:
+                notLost.append(key)
+            if values == 1:
+                oneLoss.append(key)
+        return(sorted(notLost), sorted(oneLoss))
