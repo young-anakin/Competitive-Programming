a2406ef3d61723e744c2e998d99af9d13e7a7c25
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-03-05 09:40:34 +0300

    Add solution for Subsets

---
+++ mysubmissions/Micheal/leetcode/subsets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        tot = []
+
+        bucket = []
+        def backtrack(i):
+            if i >= len(nums):
+                tot.append(bucket[::])
+                return 
+            
+            bucket.append(nums[i])
+
+            backtrack(i+1)
+
+            bucket.pop()
+
+            backtrack(i+1)
+        backtrack(0)
+        return tot
+
+
+            
